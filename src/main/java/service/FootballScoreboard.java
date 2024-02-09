3c7fccf6e24ab6cbc9485da22b4ce3645a2393b5
Oluwaseun Smart <smartunlimited95@gmail.com>
2024-02-09 10:17:04 +0100

    rename generic scoreboard service name to footballscoreboard to be more precise

---
+++ src/main/java/service/FootballScoreboard.java
@@ -0,0 +1,15 @@
+package service;
+
+import model.Match;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FootballScoreboard {
+
+    private List<Match> matches;
+
+    public FootballScoreboard() {
+        matches = new ArrayList<>();
+    }
+}
