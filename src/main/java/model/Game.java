b0d905e63959cbfe807d75a2fcf480d0f3c89e7b
Oluwaseun Smart <smartunlimited95@gmail.com>
2024-02-09 10:07:54 +0100

    Init the sportradar scoreboard service

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,4 +1,52 @@
 package model;
 
+import java.util.Date;
+
 public class Game {
+
+    private String homeTeam;
+    private String awayTeam;
+    private int homeScore;
+    private int awayScore;
+    private Date startTime;
+
+    public Game(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+        this.startTime = new Date();
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setScore(int homeScore, int awayScore) {
+        if (homeScore < 0 || awayScore < 0) {
+            throw new IllegalArgumentException("Scores cannot be negative.");
+        }
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    public int getTotalScore() {
+        return homeScore + awayScore;
+    }
 }
