079244ab06a57e24ba354f3832dad6e60e4f9a94
Oluwaseun Smart <smartunlimited95@gmail.com>
2024-02-09 10:24:26 +0100

    added test case to start and update a match

--- src/test/java/FootballScoreboardTest.java
+++ src/test/java/FootballScoreboardTest.java
@@ -19,4 +19,12 @@ public class FootballScoreboardTest {
         assertEquals(1, scoreboard.getSummary().size());
     }
 
+    @Test
+    public void testUpdateScore() {
+        scoreboard.startMatch("Nigeria", "Poland");
+        scoreboard.updateScore("France", "Germany", 2, 1);
+        assertEquals(2, scoreboard.getSummary().get(0).getHomeScore());
+        assertEquals(1, scoreboard.getSummary().get(0).getAwayScore());
+    }
+
 }
