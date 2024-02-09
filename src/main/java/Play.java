346b3fe654682fb3a59f0763b05fed999d33ca5a
Oluwaseun Smart <smartunlimited95@gmail.com>
2024-02-09 12:30:45 +0100

    clean Play class

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -1,22 +1,5 @@
-import service.FootballScoreboard;
-
 public class Play {
     public static void main(String[] args) {
-        System.out.println("Live scores");
-
-        FootballScoreboard scoreboard = new FootballScoreboard();
-        scoreboard.startMatch("Mexico", "Canada");
-        scoreboard.updateScore("Mexico", "Canada", 0, 5);
-        scoreboard.startMatch("Spain", "Brazil");
-        scoreboard.updateScore("Spain", "Brazil", 10, 2);
-        scoreboard.startMatch("Germany", "France");
-        scoreboard.updateScore("Germany", "France", 2, 2);
-        scoreboard.startMatch("Uruguay", "Italy");
-        scoreboard.updateScore("Uruguay", "Italy", 6, 6);
-        scoreboard.startMatch("Argentina", "Australia");
-        scoreboard.updateScore("Argentina", "Australia", 3, 1);
-
-        System.out.println("Live");
-        scoreboard.getSummary().forEach(m -> System.out.println(m));
+        System.out.println("Live Football Scores");
     }
 }
