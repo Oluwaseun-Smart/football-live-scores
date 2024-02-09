c90ceb22df42875968eb0ae714fc9cd841424295
Oluwaseun Smart <smartunlimited95@gmail.com>
2024-02-09 10:22:07 +0100

    added test class for the scoreboard

--- src/test/java/FootballScoreboardTest.java
+++ src/test/java/FootballScoreboardTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import service.FootballScoreboard;
+
+import static org.junit.Assert.assertEquals;
+
 public class FootballScoreboardTest {
+
+    private FootballScoreboard scoreboard;
+
+    @Before
+    public void setUp() {
+        scoreboard = new FootballScoreboard();
+    }
+
+    @Test
+    public void testStartMatch() {
+        scoreboard.startMatch("Nigeria", "Poland");
+        assertEquals(1, scoreboard.getSummary().size());
+    }
+
 }
