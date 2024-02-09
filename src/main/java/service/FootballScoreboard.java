10ff3078d3d562386a5d959f85a001f67bbe331d
Oluwaseun Smart <smartunlimited95@gmail.com>
2024-02-09 11:51:17 +0100

    fix failing test cases

--- src/main/java/service/FootballScoreboard.java
+++ src/main/java/service/FootballScoreboard.java
@@ -5,6 +5,7 @@ import model.Match;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class FootballScoreboard {
 
@@ -18,7 +19,7 @@ public class FootballScoreboard {
         if (homeTeam == null || awayTeam == null || homeTeam.isEmpty() || awayTeam.isEmpty()) {
             throw new IllegalArgumentException("Home team and away team names cannot be null or empty.");
         }
-        if (findMatch(homeTeam, awayTeam) != null) {
+        if (findMatch(homeTeam, awayTeam).isPresent()) {
             throw new IllegalArgumentException("A match between these teams is already in progress.");
         }
         Match match = new Match(homeTeam, awayTeam);
@@ -27,22 +28,21 @@ public class FootballScoreboard {
     }
 
     public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
-        Match match = findMatch(homeTeam, awayTeam);
+        Match match = findMatch(homeTeam, awayTeam).orElseThrow(() -> new IllegalArgumentException(String.format("No match found between %s and %s.", homeTeam, awayTeam)));
         match.setScore(homeScore, awayScore);
         System.out.println(String.format("Score updated for match between %s and %s.", homeScore, awayScore));
     }
 
     public void finishMatch(String homeTeam, String awayTeam) {
-        Match match = findMatch(homeTeam, awayTeam);
+        Match match = findMatch(homeTeam, awayTeam).orElseThrow(() -> new IllegalArgumentException(String.format("No match found between %s and %s.", homeTeam, awayTeam)));
         matches.remove(match);
         System.out.println("Match between " + homeTeam + " and " + awayTeam + " finished.");
     }
 
-    private Match findMatch(String homeTeam, String awayTeam) {
+    private Optional<Match> findMatch(String homeTeam, String awayTeam) {
         return matches.stream()
                 .filter(match -> match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(String.format("No match found between %s and %s.", homeTeam, awayTeam)));
+                .findFirst();
     }
 
     public List<Match> getSummary() {
